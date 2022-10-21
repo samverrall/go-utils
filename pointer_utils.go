202b203b55efd03b2381e3ae0307f70df8953273
Sam Verrall <samvezza@hotmail.co.uk>
2022-10-21 23:09:06 +0100

    feat(pointer_utils): add get pointer deferenced value func

--- pointer_utils.go
+++ pointer_utils.go
@@ -4,3 +4,13 @@ package goutils
 func ToPointer[T any](value T) *T {
 	return &value
 }
+
+// PointerVal returns the deferenced pointer of v, a pointer. If the pointer
+// is nil the zerovalue is returned.
+func PointerVal[T any](v *T) T {
+	if v == nil {
+		var zeroVal T
+		return zeroVal
+	}
+	return *v
+}
